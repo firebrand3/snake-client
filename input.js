ebd2cfb5413621b8a1c56a7dcfbc4916e6001a33
Asim <asim.awan@outlook.com>
2021-08-21 01:40:12 +0000

    added logic to setupInput function to move snake using wasd keys

--- input.js
+++ input.js
@@ -1,7 +1,10 @@
 const net = require("net");
 const { connect } = require("./client");
 
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,12 +13,24 @@ const setupInput = function () {
   const handleUserInput = function (key) {
     if (key === "\u0003") {
       process.exit();
-  }
-}
+    }
+    if (key === "w") {
+      connection.write("Move: up");
+    }
+    if (key === "a") {
+      connection.write("Move: left");
+    }
+    if (key === "d") {
+      connection.write("Move: right");
+    }
+    if (key === "s") {
+      connection.write("Move: down");
+    }
+  };
   stdin.on("data", (key) => {
     handleUserInput(key);
-    });
+  });
   return stdin;
 };
 
-module.exports = {setupInput};
+module.exports = { setupInput };
