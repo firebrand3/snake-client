37aec96b96f07b2bb5049d174e6fe44bdfb1179a
Asim <asim.awan@outlook.com>
2021-08-21 01:36:12 +0000

    linted

--- client.js
+++ client.js
@@ -1,18 +1,18 @@
 const net = require("net");
 
-const connect = function() {
+const connect = function () {
   let delay = 0;
 
   const conn = net.createConnection({
-    host: "localhost",// IP address here,
-    port: 50541// PORT number here,
+    host: "localhost", // IP address here,
+    port: 50541, // PORT number here,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on('connect', () => {
-    conn.write('Name: AMA');
+  conn.on("connect", () => {
+    conn.write("Name: AMA");
     // setInterval(() => {
     //   conn.write("Move: up");
     // }, 200);  // conn.write("Move: up");
@@ -21,19 +21,18 @@ const connect = function() {
     // conn.write("Move: right");
   });
 
-//  conn.on('connect', () => {
-//     conn.write("Move: up");
-//     conn.write("Move: down");
-//     conn.write("Move: left");
-//     conn.write("Move: right");
-//   });
+  //  conn.on('connect', () => {
+  //     conn.write("Move: up");
+  //     conn.write("Move: down");
+  //     conn.write("Move: left");
+  //     conn.write("Move: right");
+  //   });
 
-  
- conn.on('data', (data) => {
+  conn.on("data", (data) => {
     console.log("you ded cuz you idled");
   });
 
   return conn;
 };
 
-module.exports = {connect};
+module.exports = { connect };
